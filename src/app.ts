dd837a3b6b4dca34eeb8bc74d240a345f0636c7a
Vignesh <balajivignesh021@gmail.com>
2023-12-09 14:33:37 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import fastify from "fastify";
+import cors from '@fastify/cors';
 import router from "./router";
 
 const server = fastify({
   // Logger only for production
   logger: !!(process.env.NODE_ENV !== "development"),
 });
+
+// Cors
+server.register(cors, { 
+  // put your options here
+  preflightContinue: true
+})
 
 // Middleware: Router
 server.register(router);
